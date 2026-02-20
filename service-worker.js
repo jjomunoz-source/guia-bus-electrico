5c75703b567f0a145a44b70621f4fa17219e44c1
jjomunoz-source <jjomunoz@gmail.com>
2026-02-20 20:28:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bus-electrico-v12";
+const CACHE_NAME = "bus-electrico-v13";
 
 const FILES_TO_CACHE = [
   "./",
@@ -9,18 +9,22 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
   self.clients.claim();
@@ -28,31 +32,11 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
-
-function abrirRecorrido(nombre) {
-  ocultarModulos();
-
-  const seccion = document.createElement("section");
-  seccion.className = "modulo";
-  seccion.innerHTML = `
-    <h2>Recorrido ${nombre}</h2>
-    <button onclick="mostrarCalle('${nombre}')">📍 Calle a Calle</button>
-    <button onclick="mostrarMapa('${nombre}')">🗺 Mapas y Desvíos</button>
-    <button onclick="volverRecorridos()">⬅ Volver</button>
-  `;
-
-  document.querySelector("main").appendChild(seccion);
-}
-
-function volverRecorridos() {
-  location.reload();
-}
-
-
-
 
 
 
