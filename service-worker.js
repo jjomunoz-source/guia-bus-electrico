cc8fb82b6c37e0e3f0b8db1af64eb638de19d8a8
jjomunoz-source <jjomunoz@gmail.com>
2026-07-28 03:32:05 -0400

    Estabiliza guía operativa STU

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
-const CACHE_NAME = "bus-electrico-v17";
+const CACHE_NAME = "guia-stu-v18";
 
 const FILES_TO_CACHE = [
   "./",
+  "./index.html",
   "./style.css",
+  "./app.js",
   "./manifest.json",
+  "./logo-stu.png",
   "./icon-192.png",
   "./icon-512.png"
 ];
@@ -31,11 +34,25 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
+        const copia = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copia));
         return response;
       })
-      .catch(() => caches.match(event.request))
+      .catch(() => caches.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+        return Response.error();
+      }))
   );
 });
